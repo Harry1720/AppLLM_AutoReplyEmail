2a414fb193f9f9cec7c3749ee2fe6abd976f281a
ThaiBinh2004 <pntb2004@gmail.com>
2025-11-28 14:47:17 +0700

    change main

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ from app.api.auth_router import router as auth_router
 from app.api.email_router import email_router
 from app.api.user_router import user_router
 app = FastAPI(
-    title="My FastAPI App",
-    description="Demo FastAPI with Swagger UI",
+    title="My AutoReplyEmail App",
+    description="Demo AutoReplyEmail with Swagger UI",
     version="1.0.0"
 )
 
